fe710852fd0c2f88eeff7691938a4a6a04a183cf
Sean Shannon <seans887@gmail.com>
2014-12-22 22:44:39 -0500

    Adding foundation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require foundation
 //= require underscore
 //= require backbone
 //= require ./app_bootstrap
@@ -19,3 +20,5 @@
 //= require_tree ./collections
 //= require_tree ./views
 //= require_tree ./routers
+
+$(function(){ $(document).foundation(); });
