c500c708f717d84132fd7e4e8b4b3b29fbc30b14
Sean Shannon <seans887@gmail.com>
2015-02-07 13:04:38 -0500

    Adding seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,39 +1,50 @@
-users = User.create([
-  { :email => "mike@fink.com", :password => "jetsrule", :first_name => "Mike", :last_name => "Fink", :couple_id => couples.first.id },
-  { :email => "megan@fink.com", :password => "lilabeast", :first_name => "Megan", :last_name => "Fink", :couple_id => couples.first.id },
-  { :email => "seans887@gmail.com", :password => "password", :first_name => "Sean" }
-])
-
+Registry.destroy_all
 registries = Registry.create([
-  { :name => "Megan and Mike's Wedding Registry", :couple_id => couples.first.id }
+  { :name => "Megan and Mike's Wedding Registry", :url_slug => "megan-and-mike", :postal_code => "11733" }
 ])
 
+User.destroy_all
+users = User.create([
+  { :email => "mike@fink.com", :password => "jetsrule", :first_name => "Mike", :last_name => "Fink", :registry_id => registries.first.id },
+  { :email => "megan@fink.com", :password => "lilabeast", :first_name => "Megan", :last_name => "Fink", :registry_id => registries.first.id },
+  { :email => "seans887@gmail.com", :password => "password", :first_name => "Sean", :last_name => "Shannon" }
+])
+
+Organization.destroy_all
 orgs = Organization.create([
   { :name => "Charity Water", :admin_id => users.third.id }
 ])
 
+Project.destroy_all
 projects = Project.create([
-  { :name => "Build a Well in Ghana", :country => "Ghana", :organization_id => orgs.first.id  }
+  { :name => "Build a Well in 0", :country => "Ghana", :organization_id => orgs.first.id  },
+  { :name => "Build a Well in 1", :country => "Ghana", :organization_id => orgs.first.id  },
+  { :name => "Build a Well in 2", :country => "Ghana", :organization_id => orgs.first.id  },
+  { :name => "Build a Well in 3", :country => "Ghana", :organization_id => orgs.first.id  },
+  { :name => "Build a Well in 4", :country => "Ghana", :organization_id => orgs.first.id  },
+  { :name => "Build a Well in 5", :country => "Ghana", :organization_id => orgs.first.id  },
+  { :name => "Build a Well in 6", :country => "Ghana", :organization_id => orgs.first.id  },
+  { :name => "Build a Well in 7", :country => "Ghana", :organization_id => orgs.first.id  }
 ])
 
-RegistryProject.create([
-  { :registry_id => registries.first.id, :project_id => projects.first.id }
-])
-
-item_types = ItemType.create([
-  { :name => "Shovel", :price => 20.99, :project_id => projects.first.id },
-  { :name => "Bag of cement", :price => 40.00, :project_id => projects.first.id },
-  { :name => "Filter", :price => 100.00, :project_id => projects.first.id }
-])
-
-purchases = Purchase.create([
-  { :user_id => users.last.id }
-])
-
-Item.create([
-  { :name => "Shovel #1", :item_type_id => item_types.first.id, :purchase_id => purchases.first.id },
-  { :name => "Shovel #2", :item_type_id => item_types.first.id },
-  { :name => "Bag of cement #1", :item_type_id => item_types.second.id, :purchase_id => purchases.first.id },
-  { :name => "Bag of cement #2", :item_type_id => item_types.second.id },
-  { :name => "Filter #1", :item_type_id => item_types.third.id }
-])
+# RegistryProject.create([
+#   { :registry_id => registries.first.id, :project_id => projects.first.id }
+# ])
+#
+# item_types = ItemType.create([
+#   { :name => "Shovel", :price => 20.99, :project_id => projects.first.id },
+#   { :name => "Bag of cement", :price => 40.00, :project_id => projects.first.id },
+#   { :name => "Filter", :price => 100.00, :project_id => projects.first.id }
+# ])
+#
+# purchases = Purchase.create([
+#   { :user_id => users.last.id }
+# ])
+#
+# Item.create([
+#   { :name => "Shovel #1", :item_type_id => item_types.first.id, :purchase_id => purchases.first.id },
+#   { :name => "Shovel #2", :item_type_id => item_types.first.id },
+#   { :name => "Bag of cement #1", :item_type_id => item_types.second.id, :purchase_id => purchases.first.id },
+#   { :name => "Bag of cement #2", :item_type_id => item_types.second.id },
+#   { :name => "Filter #1", :item_type_id => item_types.third.id }
+# ])
