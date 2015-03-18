b6c149fc2fb2f37ad94ead6da36cdf475e68567f
Corey Speisman <Cspeisman@gmail.com>
2015-03-17 22:35:50 -0400

    more site tweaks

--- app/helpers/registries_helper.rb
+++ app/helpers/registries_helper.rb
@@ -6,6 +6,9 @@ module RegistriesHelper
     end
   end
 
+  def calc_percentage(registry)
+    (registry.items.purchased.count.to_f / registry.items.count.to_f).to_i
+  end
   def approved_project?(registry, project)
     RegistryProject.where(:registry_id => registry.id, :project_id => project.id).first.approved 
   end
