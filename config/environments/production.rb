a82b949eb1956c4d7fa361ec797682e9ba139c00
Sean Shannon <seans887@gmail.com>
2015-04-02 16:17:34 -0500

    Forcing SSL because it works nowwww

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,7 +39,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Set to :debug to see everything in the log.
   config.log_level = :info
@@ -63,7 +63,7 @@ Rails.application.configure do
   # Action Mailer Configs
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  
+
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { :host => 'wwww.heartful.ly' }
   config.action_mailer.delivery_method = :smtp
