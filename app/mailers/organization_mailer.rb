799b336dad79d6ff68a9d5700d815cc93e553399
Cspeisman <Cspeisman@gmail.com>
2015-06-03 21:32:44 -0400

    added from email address for mailer

--- app/mailers/organization_mailer.rb
+++ app/mailers/organization_mailer.rb
@@ -1,4 +1,6 @@
 class OrganizationMailer < ActionMailer::Base
+  default from: "concierge@heartful.ly"
+  
 	def notify_admin(organization)
 		@organization = organization
 		mail to: 'partnerships@heartful.ly, corey@heartful.ly', subject: "New Org Application - id: #{@organization.id}"
