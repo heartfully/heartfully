e9d4ff9dafaf2b67ed898dcc11d433163ebdab7e
Cspeisman <cspeisman@gmail.com>
2015-11-23 19:31:02 -0500

    iron out admin stuff

--- app/models/registry.rb
+++ app/models/registry.rb
@@ -35,14 +35,9 @@ end
 #  id                         :integer          not null, primary key
 #  name                       :string(255)      not null
 #  url_slug                   :string(255)      not null
-#  banner_img                 :string(255)
-#  profile_img                :string(255)
 #  description                :text
 #  address_1                  :string(255)
 #  address_2                  :string(255)
-#  city                       :string(255)
-#  state                      :string(255)
-#  postal_code                :string(255)      not null
 #  country                    :string(255)
 #  created_at                 :datetime
 #  updated_at                 :datetime
@@ -56,4 +51,6 @@ end
 #  profile_image_content_type :string(255)
 #  profile_image_file_size    :integer
 #  profile_image_updated_at   :datetime
+#  city_state                 :string(255)
+#  wedding_date               :date
 #
