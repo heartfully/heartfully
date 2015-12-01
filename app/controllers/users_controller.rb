ee936e9a8f510688a39deb913479b4d48695d2fb
Cspeisman <cspeisman@gmail.com>
2015-11-30 19:09:42 -0500

    welcome email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,7 +33,7 @@ class UsersController < ApplicationController
     if @user.save
       sign_in @user
       partner_invite.update(:accepted_at => Time.zone.now) if partner_invite
-      # UserMailer.user_confirmation(@user).deliver!
+      UserMailer.user_confirmation(@user).deliver!
       # flash[:notice] = "Account created. We'll update you when more projects are available!"
       redirect_to user_path(@user)
     else
