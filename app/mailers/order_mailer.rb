9aab96b0d04098be7d512c395f2426737b73cd66
Erik Wisell <erik.wisell1016@gmail.com>
2016-02-06 04:07:19 +0100

    modify the content of email in valentine page

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,10 +1,10 @@
 class OrderMailer < ActionMailer::Base
   add_template_helper(OrdersHelper)
-  default from: "concierge@heartful.ly"
+  default from: "Heartful.ly@heartful.ly"
 
   def order_confirmation(order)
     @order = order
-    mail to: "#{order.email}, tech@heartful.ly", subject: "Your Heartful.ly gift has been delivered! "
+    mail to: "#{order.email}", subject: "Your Heartful.ly gift has been delivered! "
   end
 
   def registry_order(order)
@@ -14,12 +14,12 @@ class OrderMailer < ActionMailer::Base
 
   def registry_order_valentine_sender(order)
     @order = order
-    mail to: "#{order.email}", subject: "You just donated a gift off registry - #{@order.registry.name} from Heartful.ly!"
+    mail to: "#{order.email}", subject: "Good news! Your generous donation is on its way!"
   end
 
   def registry_order_valentine_receiver(order)
     @order = order
-    mail to: "#{order.recipient_email}", subject: "You just received a gift off registry - #{@order.registry.name} from Heartful.ly!"
+    mail to: "#{order.recipient_email}", subject: "You’ve received a Valentine’s Day gift from #{@order.first_name}"
   end
 
   def email_kate(order)
