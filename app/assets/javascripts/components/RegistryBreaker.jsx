6dd712a455ec72e7b09520c6e14cb66a8f7163fe
Erik Wisell <erik.wisell1016@gmail.com>
2016-02-12 04:38:41 +0100

    change text on projects page

--- app/assets/javascripts/components/RegistryBreaker.jsx
+++ app/assets/javascripts/components/RegistryBreaker.jsx
@@ -2,7 +2,7 @@ class RegistryBreaker extends React.Component {
   render() {
     return (
       <div className="c-breaker dark-purple center mb--large">
-        <h3 className="roboto large mb">Heartful.ly is free for couples. Free for nonprofits.</h3>
+        <h3 className="roboto large mb">Share your love where it's needed most.</h3>
         <a href="/registries/new" className="c-button c-button--primary">Create a registry</a>
       </div>
     );
