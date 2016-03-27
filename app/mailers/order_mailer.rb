1a8786b284986ebe829c974a75e83e3233c65ae9
Alexis Johnson <alexis.johnson052@gmail.com>
2016-03-27 13:07:43 -0400

    build out campaign emails

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -13,15 +13,15 @@ class OrderMailer < ActionMailer::Base
     mail to: "#{order.registry.email}, tech@heartul.ly", subject: "#{@order.first_name} #{@order.last_name} just purchased a gift off your Heartful.ly registry!"
   end
 
-  def registry_order_valentine_sender(order)
-    @order = order
-    mail to: "#{order.email}", subject: "Good news! Your generous donation is on its way!"
-  end
-
-  def registry_order_valentine_receiver(order)
-    @order = order
-    mail to: "#{order.recipient_email}", subject: "You’ve received a Valentine’s Day gift from #{@order.first_name}"
-  end
+  # def registry_order_valentine_sender(order)
+  #   @order = order
+  #   mail to: "#{order.email}", subject: "Good news! Your generous donation is on its way!"
+  # end
+  #
+  # def registry_order_valentine_receiver(order)
+  #   @order = order
+  #   mail to: "#{order.recipient_email}", subject: "You’ve received a Valentine’s Day gift from #{@order.first_name}"
+  # end
 
   def campaign_order_sender(order)
     @order = order
@@ -30,7 +30,12 @@ class OrderMailer < ActionMailer::Base
 
   def campaign_order_receiver(order)
     @order = order
-    mail to: "#{order.recipient_email}", subject: "You’ve received a gift from #{@order.first_name}"
+    mail to: "#{order.recipient_email}", subject: "#{@order.registry.description}"
+  end
+
+  def campaign_order_kate(order)
+    @order = order
+    mail to: 'kate@heartful.ly, tech@heartul.ly', subject: "Order #{@order.id} summary"
   end
 
   def email_kate(order)
