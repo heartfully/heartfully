361fcbb84246bf00edc1b98e7dd4c54a0b859ad8
Alexis Johnson <alexis.johnson052@gmail.com>
2016-04-03 20:40:02 -0400

    Revert "changes form from wedding_date to event_date"
    This reverts commit 528ee23f39f036963d62332e7c456b6216ed5cb4.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160403141032) do
+ActiveRecord::Schema.define(version: 20160205035241) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -231,7 +231,7 @@ ActiveRecord::Schema.define(version: 20160403141032) do
   create_table "registries", force: true do |t|
     t.string   "name",                       null: false
     t.string   "url_slug",                   null: false
-    t.text     "campaign_message"
+    t.text     "description"
     t.string   "address_1"
     t.string   "address_2"
     t.string   "country"
@@ -248,10 +248,8 @@ ActiveRecord::Schema.define(version: 20160403141032) do
     t.integer  "profile_image_file_size"
     t.datetime "profile_image_updated_at"
     t.string   "city_state"
-    t.date     "event_date"
+    t.date     "wedding_date"
     t.string   "email"
-    t.string   "type"
-    t.string   "tweet"
   end
 
   add_index "registries", ["url_slug"], name: "index_registries_on_url_slug", unique: true, using: :btree
