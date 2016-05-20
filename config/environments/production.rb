c7e1ef5db85a50ac114e0efe2f92405a6584df71
Alex Leach <aleach84@gmail.com>
2016-05-20 10:49:57 -0400

    Alex: serve assets under https until I figure out why everything is under https

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
   # S3 and Paperclip settings
   config.paperclip_defaults = {
     :storage => :s3,
-    :s3_protocol => 'http',
+    :s3_protocol => 'https',
     :s3_credentials => {
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
