f896d93716523e3c1a73e6a407cc269c48cad71a
Alex Leach <aleach84@gmail.com>
2016-07-30 23:48:53 -0500

    Alex: press bar on homepage

--- app/assets/javascripts/media-logo.js
+++ app/assets/javascripts/media-logo.js
@@ -6,3 +6,12 @@ $(".c-icon--media-hidden").mouseleave( function(){
   $(this).css("display", "none")
   $(this).prev().css("display", "inline-block");
 })
+
+$(".c-icon--media-bar-grey").mouseenter(function(){
+  $(this).css("display", "none")
+  $(this).next().css("display", "inline-block");
+})
+$(".c-icon--media-bar-purple").mouseleave( function(){
+  $(this).css("display", "none")
+  $(this).prev().css("display", "inline-block");
+})
