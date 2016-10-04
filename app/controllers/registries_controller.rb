afcbb75fa8828dfd6e996a847b383bb57470812a
Alex Leach <aleach84@gmail.com>
2016-10-04 14:28:18 -0500

    Alex: fix registry bug, add tax-deductible status to gift items on birthdays

--- app/controllers/registries_controller.rb
+++ app/controllers/registries_controller.rb
@@ -9,8 +9,10 @@ class RegistriesController < ApplicationController
   # GET /registry/:url_slug
   def show
     if @registry
-      redirect_to campaign_home_path(@registry.url_slug) if @registry.type.downcase == "campaign"
-      redirect_to birthday_home_path(@registry.url_slug) if @registry.type.downcase == "birthday"
+      if @registry.type
+        redirect_to campaign_home_path(@registry.url_slug) if @registry.type.downcase == "campaign"
+        redirect_to birthday_home_path(@registry.url_slug) if @registry.type.downcase == "birthday"
+      end
       redirect_to project_registry_form_path(@registry) if @registry.projects.empty?
     else
       redirect_to root_path
